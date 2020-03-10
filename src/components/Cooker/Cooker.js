e656a78b53175f66b0cef924f13d5eb355c6fd44
Zach Maynard <maynard.zach23@gmail.com>
2020-03-10 12:16:26 -0400

    skeleton

--- src/components/Cooker/Cooker.js
+++ src/components/Cooker/Cooker.js
@@ -6,65 +6,14 @@ let hangry = 'I am HANGRY!';
 
 const Cooker = () => {
 
-    console.log('component rendered');
-
     const [stoveTemp, setStoveTemp] = useState(0);
     const [boiling, setBoiling] = useState(false);
     const [cooked, setCooked] = useState(false);
     const [eaten, setEaten] = useState(false);
 
-    function Pot() {
-        this.startBoiling = function() {
-            if(stoveTemp >= 400){
-                setBoiling(true);
-            } else {
-                alert('Burner not hot enough yet!')
-            }
-        }
-    };
-
-    let increment = () => {
-        setStoveTemp(stoveTemp + 50);
-    };
-
-    let decrement = () => {
-        if(stoveTemp == 0){
-            setStoveTemp(0);
-            alert("Can't set temperature below 0!")
-        } else {
-            setStoveTemp(stoveTemp - 50);
-        }
-    }
-
-    let handleBoil = () => {
-        setCooked(false);
-        setEaten(false);
-        let pastaPot = new Pot();
-        pastaPot.startBoiling();
-        console.log(pastaPot);
-    };
-
-    useEffect(() => {
-        if(boiling === true){
-            document.title = 'Cooking...'
-            setTimeout(() => {
-                alert('Tada! Your pasta finished cooking!');
-                setBoiling(false);
-                setCooked(true);
-                document.title = 'Cooked!!!';
-            }, 5000);
-        }
-    }, [boiling]);
-
     return (
         <div>
-            <button onClick={() => decrement()}>- 50 degrees</button>
-            <button onClick={increment}>+ 50 degrees</button>
-            <p>{stoveTemp}</p>
-            <button onClick={handleBoil}>Start Boiling!</button>
 
-            {cooked === true && eaten === false ? <Table setEaten={setEaten} hangry={hangry} /> : null}
-            {cooked === true && eaten === true ? <img src={Full} /> : null}
         </div>
     )
 }
--- src/components/Cooker/Table/Table.js
+++ src/components/Cooker/Table/Table.js
@@ -3,19 +3,10 @@ import './Table.css';
 import Pasta from '../../../assets/pasta.jpg';
 
 const Table = (props) => {
-    console.log('props object', props);
-
-    const [satisfied, setSatisfied] = useState(props.hangry);
-    console.log(satisfied);
-
-    let handleChange = () => {
-        props.setEaten(true);
-    }
 
     return(
         <div>
-            <h3>{satisfied}</h3>
-            <img src={Pasta} onClick={handleChange} />
+
         </div>
     )
 }
